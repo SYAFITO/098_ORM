1e57bc623b5351bb1107a7e11f6e4960c900043f
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2025-10-27 10:00:06 +0700

    menambahkan method post

--- index.js
+++ index.js
@@ -31,13 +31,4 @@ db.sequelize.sync()
         }
     });
 
-    app.get('/komik', async (req, res) => {
-        try {
-            const komik = await db.Komik.findAll();
-            res.send(komik);
-        }catch (err) {
-            res.send(err);
-        }
-    });
-
     
